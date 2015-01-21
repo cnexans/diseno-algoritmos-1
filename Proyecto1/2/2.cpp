3ef59fd7a6b96bb76e4da5e0e2929f95eefd3695
Fabio Castro <fabio@cesma.usb.ve>
2015-01-21 15:58:59 -0430

    Movido problema 3 a su carpeta, antes decia 1

--- Proyecto1/2/2.cpp
+++ Proyecto1/2/2.cpp
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <vector>
+#include <iostream>
+#include <stack>
+#include <algorithm>
 using namespace std;
 
 int main(){
-	
+	int cant_casos;
+	int cant_oficinas;
+	int cant_querys;
+	stack<pair<int,int> > pila_arcos;
+
+	// Lectura de cantidad de casos 
+
+	cin >> cant_casos;
+	cant_oficinas
+
 }
