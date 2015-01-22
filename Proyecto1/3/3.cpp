1d0f42ebaef20261e871d0d1763e2e9bd942063a
Leopoldo Pimentel <leofaster@gmail.com>
2015-01-21 23:16:36 -0430

    Cambio los comentarios del 3er codigo, se agrega el pseudocodigo @fabiocasmar

--- Proyecto1/3/3.cpp
+++ Proyecto1/3/3.cpp
@@ -6,44 +6,47 @@ using namespace std;
 
 int main(){
 	// Decalaracion de Variables
-	int cant_empleados = 0;
-	int cant_actividades = 0;
+	int ultimo;
 	int desde = 0;
 	int hasta = 0;
-	int ultimo;
 	int total = 0;
-	vector<pair<int,int> > actividades;
+	int cant_empleados = 0;
+	int cant_actividades = 0;
+	vector < pair < int, int > > actividades;
 
-	// Leo la cantidad de Empleados
+	// Leemos la cantidad de Empleados
 	cin >> cant_empleados;
 	
-	// Hago el procedimiento por cada empleado de DACE
-	while(cant_empleados > 0){
-		total=0;
+	// Hacemos el procedimiento por cada empleado de DACE
+	while(cant_empleados > 0)
+	{
+		total = 0;
 		cin >> cant_actividades;
 		actividades.clear();
-		// Leo las actividades de dicho empleado de DACE
+		// Leemos las actividades de dicho empleado de DACE
 
-		while(cant_actividades > 0){
+		while( cant_actividades > 0 ){
 			cin >> desde >> hasta;
-			actividades.push_back(make_pair(desde,hasta));
-			cant_actividades-=1;
+			actividades.push_back(make_pair(desde, hasta));
+			cant_actividades -= 1;
 		}
 
 		ultimo = -1;
-		// Ordeno el Vector
-		sort(actividades.begin(), actividades.end());
+		// Ordenamos el Vector
+		sort( actividades.begin(), actividades.end() );
 
-		for (int i=1; i < actividades.size(); i++){
-			if(ultimo > actividades[i].first){
-				total+=1;
+		for ( int i = 1; i < actividades.size(); i++ )
+		{
+			if( ultimo > actividades[i].first )
+			{
+				total += 1;
 			}
 			ultimo = actividades[i].second; 
 		}
 
-		// Imprimo la cantidad por cada empleado
+		// Imprimimos la cantidad por cada empleado
 		cout << total << endl;
-		cant_empleados-=1;
+		cant_empleados -= 1;
 	}
 	return 0;
 }
