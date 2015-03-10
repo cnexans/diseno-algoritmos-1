4e97c9e9e43348d1952cf65ed47d01924d6ade34
Fabio Castro <fabio@cesma.usb.ve>
2015-03-10 10:59:36 -0430

    Agregados problemas BORW y BABY

--- P3/2-BABY/p2.cpp
+++ P3/2-BABY/p2.cpp
@@ -1,11 +1,63 @@
+/*
+/ ci5651 (Diseño de Algoritmos I) - Proyecto 3 - Problema BABY SPOJ
+/ Fabio, Castro, 10-10132
+/ Leopoldo, Pimentel 06-40095
+*/
+ 
+ // Librerías a importar
 #include <stdio.h>
-#include <vector>
+#include <stdlib.h>
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int main(){
+//  Máximo entero
+#define MAXN 16
+#define MAXB (1<<MAXN)
 
-return 0;
+// Variables globales para el DP
+unsigned char mascaraBit[MAXB];
+int dp[MAXB];
+int T, N;
+int origen[MAXN], destino[MAXN];
 
+// Función para calculo del mínimo
+int min(int a, int b){ 
+	return a < b ? a : b; 
+}
+
+int main(void){
+	int i, j;
+
+	// Creación de la Máscara de Bits
+	for (i = 1; i < MAXB; i++)
+		mascaraBit[i] = mascaraBit[i ^ (i&-i)] + 1;
+
+	// Ciclo que itera por cada caso de prueba hasta que N sea igual a 0,
+	// y guarda en N la cantidad de elementos de la secuencia a leer
+	while (cin >> N, N){
+
+		// Se lee los nodos X
+		for (i = 0; i < N; i++){
+			cin >> origen[i];
+		}
+
+		// Se lee los nodos Y
+		for (i = 0; i < N; i++){
+			cin >> destino[i];
+		}
+
+		// Ciclo del DP
+		for (i = 1; i < (1<<N); i++){
+			for (j = 0, dp[i] = (1<<30); (1<<j) <= i; j++){
+				if (i & (1<<j)){
+					dp[i] = min(dp[i],
+					            dp[i^(1<<j)] + abs(mascaraBit[i]-j-1) + abs(origen[j]-destino[mascaraBit[i]-1]));
+				}
+			}
+		}
+
+		// Valor de salida del DP
+		cout << dp[(1<<N) -1] << endl;
+	}
+	return 0;
 }
--- P3/3-BORW/p3.cpp
+++ P3/3-BORW/p3.cpp
@@ -1,11 +1,50 @@
+/*
+/ ci5651 (Diseño de Algoritmos I) - Proyecto 3 - Problema BORW SPOJ
+/ Fabio, Castro, 10-10132
+/ Leopoldo, Pimentel 06-40095
+*/
+
+// Librerías a importar
 #include <stdio.h>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <math.h>
 using namespace std;
 
-int main(){
+//  Máximo entero
+#define MAXN 201
 
-return 0;
+// Variables globales para el DP
+int dp[MAXN][MAXN][MAXN]; // Matriz de 3 dimensiones usada para calcular el DP
+int secuencia_num[MAXN]; // Vector donde se guarda la secuencia de números a calcular el mejor DP
+int N; // Entero donde se guarda la cantidad de elementos 
 
+int main(void){
+	int i, arriba, abajo;
+	// Ciclo que itera por cada caso de prueba hasta que N sea igual a -1,
+	// y guarda en N la cantidad de elementos de la secuencia a leer
+	while (cin >> N, N != -1){
+
+		// Se lee la secuencia de números del caso de prueba
+		for (i = 1; i <= N; i++){
+			cin >> secuencia_num[i];
+		}
+
+		// For donde se realiza el DP, tomando en cuenta las subsecuencias anteriores
+		for (i = 1; i <= N; i++){
+			for (arriba = i+1; arriba <= N+1; arriba++){
+				for (abajo = i+1; abajo <= N+1; abajo++){
+					dp[i][arriba][abajo] = dp[i-1][arriba][abajo] + 1;
+					if (abajo > N || secuencia_num[i] < secuencia_num[abajo]){
+						dp[i][arriba][abajo] = min(dp[i][arriba][abajo], dp[i-1][arriba][i]);
+					}
+					if (arriba > N || secuencia_num[i] > secuencia_num[arriba]){
+						dp[i][arriba][abajo] = min(dp[i][arriba][abajo], dp[i-1][i][abajo]);
+					}
+				}
+			}
+		}
+		// Se imprime el caso de prueba actual
+		cout << dp[N][N+1][N+1] << endl;
+	}
+	return 0;
 }
