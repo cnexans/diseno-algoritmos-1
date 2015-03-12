4f0c1803eae764fd6cb86907787caddb7bab5f2b
Leopoldo Pimentel <leofaster@gmail.com>
2015-03-12 17:59:49 -0430

    $ empezando

--- P3/4-MAXWOODS/p4.cpp
+++ P3/4-MAXWOODS/p4.cpp
@@ -4,8 +4,107 @@
 #include <algorithm>
 using namespace std;
 
+int T; // Casos de prueba 
+int m,n; // Columnas, Filas
+char data[200][200]; // Almacenar la entrada
+int dp[2][200]; // Almacenar dp
+int salida;
 int main(){
+	cin >> T;
+	while(T--){
+		int mejor = 0;
+		cin >> m >> n;
+		for(int i =0; i < m;i++){
+			dp[1][i]=0;
+			dp[2][i]=0;
+		}
+		for (int in = 0 ; in < n; in++){
+			for (int im = 0; im < m; im++){
+				cin >> data[in][im];
+			}
+			if (in%2==0){ // DERECHA
+				for(int i = 0; i < m; i++){
+					if(i!=0){
+						dp[2][i]=max(dp[2][i-1],dp[1][i]);
+					}else{
+						dp[2][i]=dp[1][i];
+					}
+					if(data[in][i]=='T'){
+						if(in==0){
+							if(dp[2][i-1]!=-1){
+								dp[2][i]=1+dp[2][i];
+							}else{
+								dp[2][i]=-1;
+							}
+						}else{	
+							if(i>0){
+								if((dp[1][i]!=-1)||(dp[2][i-1]!=-1)){
+									dp[2][i]=1+dp[2][i];
+								}else{
+									dp[2][i]=-1;
+								}
+							}else{
+								if(dp[1][i]!=-1){
+									dp[2][i]=1+dp[2][i];
+								}else{
+									dp[2][i]=-1;
+								}
+							}
+						}
+					}
+					if(data[in][i]=='#'){
+						dp[2][i]=-1;
+					}	
+				}
+			}else{ // IZQUIERDA
+				for(int i = m-1;i >= 0; i--){
+					if(i!=m-1){
+						dp[2][i]=max(dp[2][i+1],dp[1][i]);
+					}else{
+						dp[2][i]=dp[1][i];
+					}
+					if(data[in][i]=='T'){
+						if(i!=m-1){
+							if((dp[1][i]!=-1)||(dp[2][i+1]!=-1)){
+								dp[2][i]=1+dp[2][i];
+							}else{
+								dp[2][i]=-1;
+							}
+						}else{
+							if(dp[1][i]!=-1){
+								dp[2][i]=1+dp[2][i];
+							}else{
+								dp[2][i]=-1;
+							}
+						}
+					
+					}
+					if(data[in][i]=='#'){
+						dp[2][i]=-1;
+					}		
+				}
 
+			}
+			//mejor = -1;
+			for(int i =0; i < m;i++){
+				mejor = max(mejor,dp[2][i]);
+				dp[1][i]=dp[2][i];
+				dp[2][i]=0;
+			}
+			for(int i =0; i < m;i++){
+				mejor = max(mejor,dp[2][i]);
+				cout <<		dp[1][i] << " " ;
+			}
+			cout << endl;
+
+		}
+		salida = 0;
+		//salida = mejor;
+		for(int i =0; i< m; i++){
+			salida = max(salida,dp[1][i]);
+		}
+		cout << max(salida,mejor) << endl;
+	}
 return 0;
 
 }
