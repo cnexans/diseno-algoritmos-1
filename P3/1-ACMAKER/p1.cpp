72418c607309568f0a783e12c45701376e24b766
Fabio Castro <fabiocastro100@gmail.com>
2015-03-12 06:46:58 -0430

    Pruebas ACMAKER

--- P3/1-ACMAKER/p1.cpp
+++ P3/1-ACMAKER/p1.cpp
@@ -2,10 +2,17 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
 int main(){
+string t = "Banana nana nana nana";
+string s = "nana";
+string::size_type i = t.find(s);
+if (i != string::npos)
+   t.erase(i, s.length());
 
+cout << endl << t;
 return 0;
 
 }
---
+++ P3/1-ACMAKER/p2.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <sstream>
+using namespace std;
+
+string palabras_excluir[100];
+string frase_revisar[150];
+
+int main(){
+	string line;
+	istringstream linestream(line); // #include <sstream>
+	string word;
+	int cant;
+	int i;
+	int j;
+	while(cin >> cant, cant=!0){
+		i = 0;
+		while(i < cant){
+			cin >> palabras_excluir[i];
+			i=i+1;
+		}
+		bool ultimo = false;
+		while(!(ultimo)){
+			// cout << ultimo;
+			while (std::getline(cin, line)){
+				istringstream linestream(line);
+				string word;
+				j = 0;
+				while (linestream >> word){
+					frase_revisar[j] = word;
+					if(frase_revisar[j] == "CASE"){
+						cout << ultimo;
+							ultimo = true;
+							cout << ultimo;
+							break;
+					}
+					// cout << frase_revisar[j];
+					j = j + 1;
+				}
+				if(ultimo) break;
+			}	
+			if(ultimo) break;
+			
+		}
+
+	}
+	return 0;
+}
+
+
+
