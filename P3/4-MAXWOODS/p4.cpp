52e5f21635ce100f84fe4ec289083ff5ffd1f4c6
Leopoldo Pimentel <leofaster@gmail.com>
2015-03-12 19:22:03 -0430

    Arregle los problemas y sigue sin funcionar

--- P3/4-MAXWOODS/p4.cpp
+++ P3/4-MAXWOODS/p4.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 using namespace std;
 
 int T; // Casos de prueba 
@@ -12,6 +13,7 @@ int salida;
 int main(){
 	cin >> T;
 	while(T--){
+		memset(data,0,sizeof(data));
 		int mejor = 0;
 		cin >> m >> n;
 		for(int i =0; i < m;i++){
@@ -22,68 +24,101 @@ int main(){
 			for (int im = 0; im < m; im++){
 				cin >> data[in][im];
 			}
-			if (in%2==0){ // DERECHA
-				for(int i = 0; i < m; i++){
-					if(i!=0){
-						dp[2][i]=max(dp[2][i-1],dp[1][i]);
-					}else{
-						dp[2][i]=dp[1][i];
-					}
-					if(data[in][i]=='T'){
-						if(in==0){
-							if(dp[2][i-1]!=-1){
-								dp[2][i]=1+dp[2][i];
-							}else{
+			// if (data[0][0] == '#'){
+			// 	goto stop;
+			// }else{
+				if (in%2==0){ // DERECHA
+					for(int i = 0; i < m; i++){
+						if(i!=0){
+							dp[2][i]=max(dp[2][i-1],dp[1][i]);
+						}else{
+							if (data[in][i]=='#'){
 								dp[2][i]=-1;
+							}else{
+								dp[2][i]=dp[1][i];
+								
 							}
-						}else{	
-							if(i>0){
-								if((dp[1][i]!=-1)||(dp[2][i-1]!=-1)){
-									dp[2][i]=1+dp[2][i];
+						}
+						if(data[in][i]!='#'){
+							if(in==0){
+								if(dp[2][i-1]!=-1){
+									if (data[in][i]=='T'){
+										dp[2][i]=1+dp[2][i];
+									}else{
+										dp[2][i]=dp[2][i];
+									}
+								}else{
+									dp[2][i]=-1;
+								}
+							}else{	
+								if(i>0){
+									if((dp[1][i]!=-1)||(dp[2][i-1]!=-1)){
+										if (data[in][i]=='T'){
+											dp[2][i]=1+dp[2][i];
+										}else{
+											dp[2][i]=dp[2][i];
+										}
+
+									}else{
+										dp[2][i]=-1;
+									}
+								}else{
+									if(dp[1][i]!=-1){
+										if (data[in][i]=='T'){
+											dp[2][i]=1+dp[2][i];
+										}else{
+											dp[2][i]=dp[2][i];
+										}
+
+									}else{
+										dp[2][i]=-1;
+									}
+								}
+							}
+						}
+						if(data[in][i]=='#'){
+							dp[2][i]=-1;
+						}	
+					}
+				}else{ // IZQUIERDA
+					for(int i = m-1;i >= 0; i--){
+						if(i!=m-1){
+							dp[2][i]=max(dp[2][i+1],dp[1][i]);
+						}else{
+							dp[2][i]=dp[1][i];
+						}
+						if(data[in][i]!='#'){
+							if(i!=m-1){
+								if((dp[1][i]!=-1)||(dp[2][i+1]!=-1)){
+									if(data[in][i]=='T'){
+										dp[2][i]=1+dp[2][i];
+									}else{
+										dp[2][i]=dp[2][i];
+
+									}
 								}else{
 									dp[2][i]=-1;
 								}
 							}else{
 								if(dp[1][i]!=-1){
-									dp[2][i]=1+dp[2][i];
+									if(data[in][i]=='T'){
+										dp[2][i]=1+dp[2][i];
+									}else{
+										dp[2][i]=dp[2][i];
+									}
+
 								}else{
 									dp[2][i]=-1;
 								}
 							}
+						
 						}
+						if(data[in][i]=='#'){
+							dp[2][i]=-1;
+						}		
 					}
-					if(data[in][i]=='#'){
-						dp[2][i]=-1;
-					}	
-				}
-			}else{ // IZQUIERDA
-				for(int i = m-1;i >= 0; i--){
-					if(i!=m-1){
-						dp[2][i]=max(dp[2][i+1],dp[1][i]);
-					}else{
-						dp[2][i]=dp[1][i];
-					}
-					if(data[in][i]=='T'){
-						if(i!=m-1){
-							if((dp[1][i]!=-1)||(dp[2][i+1]!=-1)){
-								dp[2][i]=1+dp[2][i];
-							}else{
-								dp[2][i]=-1;
-							}
-						}else{
-							if(dp[1][i]!=-1){
-								dp[2][i]=1+dp[2][i];
-							}else{
-								dp[2][i]=-1;
-							}
-						}
-					
-					}
-					if(data[in][i]=='#'){
-						dp[2][i]=-1;
-					}		
-				}
 
+				// }
 			}
 			//mejor = -1;
 			for(int i =0; i < m;i++){
@@ -91,19 +126,21 @@ int main(){
 				dp[1][i]=dp[2][i];
 				dp[2][i]=0;
 			}
-			for(int i =0; i < m;i++){
-				mejor = max(mejor,dp[2][i]);
-				cout <<		dp[1][i] << " " ;
-			}
-			cout << endl;
+			// for(int i =0; i < m;i++){
+			// 	mejor = max(mejor,dp[2][i]);
+			// 	cout <<		dp[1][i] << " " ;
+			// }
+			// cout << endl;
 
 		}
+		// stop:
 		salida = 0;
 		//salida = mejor;
 		for(int i =0; i< m; i++){
 			salida = max(salida,dp[1][i]);
 		}
 		cout << max(salida,mejor) << endl;
+		// cout << '0' << endl;
 	}
 return 0;
 
