ea6782ebb54c35f29c27321d2d8dcde724fdf6f9
Fabio Castro <fabio@cesma.usb.ve>
2015-03-13 10:55:26 -0430

    Agregados comentarios a MAXWOODS

--- P3/4-MAXWOODS/p4.cpp
+++ P3/4-MAXWOODS/p4.cpp
@@ -1,43 +1,71 @@
-#include <stdio.h>
-#include <vector>
+/*
+/ ci5651 (Diseño de Algoritmos I) - Proyecto 3 - Problema BORW SPOJ
+/ Fabio, Castro, 10-10132
+/ Leopoldo, Pimentel 06-40095
+*/
+
+/* Se realiza una ejecución del DP bottom-up, usando un vector de memorización 
+/  2*n, donde n es el ancho de la matriz.
+*/
+
+// Librerias a usar
 #include <iostream>
-#include <algorithm>
 #include <cstring>
-#include <queue>
-#include <utility>
 using namespace std;
 
-
-int T; // Casos de prueba 
+// Declaración de variables globales
+int cant; // Casos de prueba 
 int m,n; // Columnas, Filas
 char data[200][200]; // Almacenar la entrada
-int dp[2][200]; // Almacenar dp
-int salida;
-int mejor;
+int dp[2][200]; // Vector de soluciones parciales del dp
+int mejor; //Variable donde se va guardando de manera temporal la mejor solucion
 const int infi_neg = -100000;
+
 int main(){
-	cin >> T;
-	while(T--){
+
+	//Se lee la cantidad de casos de prueba
+	cin >> cant;
+
+	// Se itera una vez por cada caso de prueba
+	while(cant--){
+
+		// Se inicializan las variables a usar
 		memset(data,0,sizeof(data));
 		mejor = 0;
 		int salida = 0;
+
+		// Se lee la cantidad de filas y de columnas del caso de prueba
 		cin >> n >> m;
+
+		// Se lee la matriz a recorreer
         for(int i=0;i<n;i++){
-            cin >> data[i];
+        	for(int j=0;j<m;j++){
+            	cin >> data[i][j];
+        	}
         }
+
+        // Se inicializa las variables del Dp en 0
         for(int j=0;j<m;j++){
             dp[1][j]=0;   
             dp[0][j]=0;            
         }
+
+        /*  Si la primera casilla tiene arbol, le marco 1 en dicha posicion
+        /   del dp
+        */
         if(data[0][0]=='T'){
         	mejor = dp[1][0] = 1;  
         }
+
+        /*  Si la primera casilla esta bloqueada, le colo infinito en dicha
+        /   posicion del dp
+        */
         if(data[0][0]=='#'){
         	mejor = 0;
         	dp[1][0] = infi_neg;
         }
      
-        //cout << (data[0][0]=='T');
+     	// Ejecuto el dp para la primera fila de la matriz
         for(int i=1;i<m;i++){
             if(data[0][i]=='#'){
                 for(int j=i;j<m;j++){
@@ -48,42 +76,32 @@ int main(){
             dp[1][i]=dp[1][i-1]+(data[0][i]=='T');
             mejor = max(mejor,dp[1][i]);
 		}
+
+		// Selecciono el mejor de dicha ejecucion
 		for(int k =0; k< m; k++){
 			mejor = max(mejor,dp[1][k]);	
-			// cout << dp[1][k] << " ";
 			dp[0][k] = dp[1][k];
 		}
-		// cout << endl;
+
+		// Ejecuto el dp para el resto de la matriz
 		for(int i=1;i<n;i++){
+			/* Selecciono como vector de soluciones al vector de
+			/  soluciones parciales anterior, y reinicializo 
+			/  el nuevo vector de soluciones parciales 
+			*/
 			for(int k =0; k< m; k++){
 				mejor = max(mejor,dp[1][k]);
-				//cout << dp[1][k] << " ";
 				dp[0][k] = dp[1][k];
 			}
-			// cout << endl;
-			if(i%2==1){
-				if(data[i][m-1]=='#'){
-			        dp[1][m-1]=infi_neg;
-		        }
-		        else{
-		        	if(data[i][m-1]=='T'){
-		            	dp[1][m-1]=dp[0][m-1]+1;
-		        	}else{
-		        		dp[1][m-1]=dp[0][m-1];
-		        	}
-		        }
-		        mejor = max(mejor,dp[1][m-1]);
-		        for(int j=m-2;j>=0;j-=1){
-		        	if(data[i][j]=='T'){
-		            	dp[1][j] = max(dp[0][j],dp[1][j+1])+1;
-		        	}else{
-		        		dp[1][j] = max(dp[0][j],dp[1][j+1]);
-		        	}
-		            if(data[i][j]=='#')
-		                dp[1][j]=infi_neg;
-		            mejor = max(mejor,dp[1][j]);
-		        }
-		    }else{
+
+			// Si estoy en una fila par
+			if(i%2==0){
+				/* Si esta bloqueado, el minimo para llegar a dicha casilla
+				/  es infinito, en caso contrario le coloco la maxima cantida
+				/  de arboles cortado para llegar a dicha posicion.
+				/  El caso borde en que se esta en la primera casilla de la 
+				/  izquierda se toma en cuenta.
+				*/
 		    	if(data[i][0]=='#'){
 			        dp[1][0]=infi_neg;
 		        }
@@ -96,31 +114,64 @@ int main(){
 		        }
 		        mejor = max(mejor,dp[1][0]);
 		        for(int j=1;j<=m-1;j+=1){
+		        	/*	Se selecciona la maxima cantidad de arboles cortados
+		        	/ de haber venido de arriba o a la izquierda, y en el caso 
+		        	/ borde solo se toma en cuenta el de arriba. En caso de
+		        	/ camino bloqueado, es infito.
+		        	*/
 		        	if(data[i][j]=='T'){
 		            	dp[1][j] = max(dp[0][j],dp[1][j-1])+1;
 		        	}else{
 		        		dp[1][j] = max(dp[0][j],dp[1][j-1]);
 		        	}
-		            if(data[i][j]=='#')
+		            if(data[i][j]=='#'){
 		                dp[1][j]=infi_neg;
-		            mejor = max(mejor,dp[1][j]);
+		            }
+		        }
+		    }else{
+		    	/* Si esta bloqueado, el minimo para llegar a dicha casilla
+				/  es infinito, en caso contrario le coloco la maxima cantida
+				/  de arboles cortado para llegar a dicha posicion.
+				/  El caso borde en que se esta en la primera casilla de la 
+				/  derecha se toma en cuenta.
+				*/
+		    	if(data[i][m-1]=='#'){
+			        dp[1][m-1]=infi_neg;
+		        }
+		        else{
+		        	if(data[i][m-1]=='T'){
+		            	dp[1][m-1]=dp[0][m-1]+1;
+		        	}else{
+		        		dp[1][m-1]=dp[0][m-1];
+		        	}
+		        }
+		        mejor = max(mejor,dp[1][m-1]);
+		        for(int j=m-2;j>=0;j-=1){
+		        	/*	Se selecciona la maxima cantidad de arboles cortados
+		        	/ de haber venido de arriba o a la izquierda, y en el caso 
+		        	/ borde solo se toma en cuenta el de arriba. En caso de
+		        	/ camino bloqueado, es infito.
+		        	*/
+		        	if(data[i][j]=='T'){
+		            	dp[1][j] = max(dp[0][j],dp[1][j+1])+1;
+		        	}else{
+		        		dp[1][j] = max(dp[0][j],dp[1][j+1]);
+		        	}
+		            if(data[i][j]=='#'){
+		                dp[1][j]=infi_neg;
+		            }
 		        }
 		    }
-		    			for(int k =0; k< m; k++){
-				// cout << dp[1][k] << " ";
-			}
-			 // cout << endl;
-
 		}
-		// stop:
-		salida = 0;
-		//salida = cu;
+
+		// Selecciono al mejor de la solucion
 		for(int i =0; i< m; i++){
-			salida = max(salida,dp[1][i]);
+			mejor = max(mejor,dp[1][i]);
 		}
-		cout << max(salida,mejor) << endl;
-		// cout << '0' << endl;
-	}
-return 0;
 
+		// Imprimo la solucion
+		cout << mejor << endl;
+	}
+
+	return 0;
 }
